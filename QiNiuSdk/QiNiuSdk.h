c6e20488769d8beacc987e65974f0699dbd96e18
郑敏 <zhengmin@zhengmins-macbook-pro-2.local>
2016-04-28 14:12:09 +0800

    Initial Commit

---
+++ QiNiuSdk/QiNiuSdk.h
@@ -0,0 +1,19 @@
+//
+//  QiNiuSdk.h
+//  QiNiuSdk
+//
+//  Created by 郑敏 on 16/4/28.
+//  Copyright © 2016年 郑敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiNiuSdk.
+FOUNDATION_EXPORT double QiNiuSdkVersionNumber;
+
+//! Project version string for QiNiuSdk.
+FOUNDATION_EXPORT const unsigned char QiNiuSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiNiuSdk/PublicHeader.h>
+
+
